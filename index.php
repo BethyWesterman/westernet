864f0ebcf0030f8faa42c2d52d0d0535d2b617a3
BethyWesterman <Bethywesterman@gmail.com>
2017-01-22 11:53:22 +0000

    Start Here

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php include 'header.php' ?>
+  
+       
+         
+           
+               <div class="container">
+        <br>
+        <br>
+        <br>
+        <h1>Home</h1>
+        
+        <p class="lead"> Welcome to westerNet- the most magical web on the internet. This website will tell you all you need to know about Walt Disney World and anything else Disney! Walt Disney World is one of the happiest places on earth and I created this website to share my complete and utter love for this amazing place. I think that even the people who don't visit often, or maybe even not at all, should have the chance to share this unique and amazing magic. On this website you can find the <a href="otherpages.com" target="_blank" title="Link to Podcasts page"> Podcasts Page </a>,<a href="otherpages.com" target="_blank" title="Link to Blogposts page">Blogposts page</a><a href="otherpages.com" target="_blank" title="Link to Contact page"> Contact page</a>. This website is pretty much self explanatory, but I have put in a little explaination, just in case.</p>
+        <p> <h3>Podcasts</h3> The podcasts page is where all the podcasts are posted. There is a new podcast every week. At the end of every month there  </p>
+    
+ 
+            <img src="http://placehold.it/200x200" alt="PLACEHOLD" class="img-round">
+            <h3>title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni aliquam officia assumenda, distinctio dicta tenetur maxime quo reiciendis doloremque animi sequi harum id sapiente eveniet officiis minus expedita voluptas quibusdam numquam facilis laboriosam. At accusantium sint asperiores error provident officiis velit praesentium, quibusdam inventore! Perferendis modi commodi quae, delectus beatae suscipit tempora corrupti minima reprehenderit velit eligendi. Aut itaque illum, beatae quaerat at rerum sed consequuntur nulla perspiciatis. Eius, dolores maxime, itaque voluptatem ad, delectus incidunt impedit a consequatur veritatis minima sunt distinctio natus harum accusantium ipsum quisquam sit provident quia sint aperiam nam ducimus! Perferendis rerum dolores optio, ipsa?</p>
+        
+        <div class="col-4">
+            <img src="http://placehold.it/200x200" alt="PLACEHOLD" class="img-round">
+            <h3>title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid dolores, ipsum dolorem, suscipit pariatur fugiat doloremque, corporis inventore obcaecati nam, cum ipsam. Est illum, eaque ad, exercitationem enim ducimus dolorem ut dicta sapiente rem ratione tempora eveniet modi, velit deserunt laboriosam eius blanditiis dolore sint natus autem, laudantium fugiat debitis! Eos alias quibusdam voluptates, nobis illum quaerat voluptate aliquid asperiores quidem perferendis. Provident voluptatem dolorum neque ipsum consequuntur nihil at quas officiis! Quaerat cupiditate sequi esse doloremque doloribus incidunt necessitatibus, quasi magnam, a quod sunt nulla quos impedit iusto asperiores ut assumenda sint molestiae beatae dicta soluta odio aut ducimus.</p>
+        </div>
+        <div>
+        <div class="col-4">
+            <img src="http://placehold.it/200x200" alt="PLACEHOLD" class="img-round">
+            <h3>title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aut voluptatibus nisi, in qui! Labore officiis, nostrum. Sit, quam aperiam ut, recusandae tenetur impedit! Ipsum ex ab facere eligendi, nisi similique nostrum, temporibus minus reprehenderit dignissimos possimus! Excepturi vero quisquam ullam veritatis harum. Necessitatibus magni maxime non nesciunt at, a possimus aspernatur laudantium provident dolores. Maiores est delectus nihil, sequi possimus neque. Magni porro saepe quam vero enim impedit! Harum doloribus ab sit consectetur nulla quis, magni suscipit dolor. Ratione inventore in itaque blanditiis, earum, cumque recusandae quis expedita modi, a labore temporibus, excepturi eveniet. Repellat pariatur delectus error tenetur.</p>
+        </div>
+    </div>
+     <div>
+        <div class="col-4">
+            <img src="http://placehold.it/200x200" alt="PLACEHOLD" class="img-round">
+            <h3>title</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Porro aut voluptatibus nisi, in qui! Labore officiis, nostrum. Sit, quam aperiam ut, recusandae tenetur impedit! Ipsum ex ab facere eligendi, nisi similique nostrum, temporibus minus reprehenderit dignissimos possimus! Excepturi vero quisquam ullam veritatis harum. Necessitatibus magni maxime non nesciunt at, a possimus aspernatur laudantium provident dolores. Maiores est delectus nihil, sequi possimus neque. Magni porro saepe quam vero enim impedit! Harum doloribus ab sit consectetur nulla quis, magni suscipit dolor. Ratione inventore in itaque blanditiis, earum, cumque recusandae quis expedita modi, a labore temporibus, excepturi eveniet. Repellat pariatur delectus error tenetur.</p>
+        </div>
+    </div>
+    <footer>
+        <div class="row clearfix">
+            <!-- /.container -->
+<?php include 'footer.php' ?>
+
+           
